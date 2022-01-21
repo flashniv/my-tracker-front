335011480b14899d483b9ab97ea40f01a53f9b51
flashniv <flashniv@gmail.com>
2022-01-21 15:06:24 +0200

    add task modify

--- src/components/ViewTaskDialog/ViewTaskDialog.js
+++ src/components/ViewTaskDialog/ViewTaskDialog.js
@@ -1,9 +1,26 @@
-import React from "react";
-import {Box, Modal, Stack} from "@mui/material";
+import React, {useState} from "react";
+import {Box, IconButton, Modal, Stack, TextField, Typography} from "@mui/material";
 import NewReleasesIcon from "@mui/icons-material/NewReleases";
 import PauseIcon from "@mui/icons-material/Pause";
 import CachedIcon from "@mui/icons-material/Cached";
 import CheckIcon from "@mui/icons-material/Check";
+import SaveIcon from '@mui/icons-material/Save';
+import APIServer from "../../API/APIServer";
+
+function getTime(row) {
+    let addSeconds=0
+    if(row.isPlay!=null) {
+        const startDate = new Date(row.isPlay)
+        addSeconds=Math.floor((Date.now() - startDate) / 1000)
+    }
+    addSeconds+=row.seconds
+    return (Math.floor(addSeconds/60)) + ':' + (addSeconds % 60 < 10 ? '0' + (addSeconds % 60) : addSeconds % 60)
+}
+
+function getSeconds(time) {
+    const [all,min,sec]=time.match('([0-9]+):([0-9][0-9])')
+    return parseInt(min)*60+parseInt(sec);
+}
 
 const getStatusIcon = function (status) {
     switch (status) {
@@ -67,6 +84,50 @@ const style = {
 };
 
 export default function ViewTaskDialog({task, updateTasks, openViewDialog, setOpenViewDialog}) {
+    const [showSave,setShowSave]=useState(false)
+    const [showTimeSave,setShowTimeSave]=useState(false)
+    const [time,setTime]=useState(getTime(task))
+    const [title,setTitle]=useState(task.title)
+    const [desc,setDesc]=useState(task.description)
+
+    const editTitle=function (e) {
+        setTitle(e.target.value)
+        setShowSave(true)
+    }
+    const editTime=function (e) {
+        if (e.target.value.match('^[0-9]+:[0-9][0-9]$')){
+            setTime(e.target.value)
+            setShowTimeSave(true)
+        }else{
+            return false;
+        }
+    }
+    const editDesc=function (e) {
+        setDesc(e.target.value)
+        setShowSave(true)
+    }
+    const saveTask=function () {
+        const updTask={
+            "project": task.project,
+            "title": title,
+            "description": desc
+        }
+        const response=APIServer.putContent("/api/task/" + task.id,updTask)
+        response.then((data)=>{
+            updateTasks()
+        },()=>{
+
+        })
+    }
+    const saveTaskTime=function () {
+        const response=APIServer.putContent("/api/task/" + task.id+"/resetPeriod?newSeconds="+getSeconds(time), {})
+        response.then((data)=>{
+            updateTasks()
+        },()=>{
+
+        })
+    }
+
     return (
         <Modal
             open={openViewDialog}
@@ -77,17 +138,99 @@ export default function ViewTaskDialog({task, updateTasks, openViewDialog, setOp
             <Stack style={style} spacing={2}>
                 <Box
                     sx={{
-                        fontWeight: "bold"
+                        display: 'flex',
                     }}
                 >
-                    {getStatusIcon(task.status)}{"  " + task.title}
+                    {getStatusIcon(task.status)}
+                    <input
+                        type="text"
+                        style={{
+                            fontSize: 'larger',
+                            border: 'none transparent',
+                            outline: 'none',
+                            marginLeft: '5px',
+                            width: '100%'
+                        }}
+                        value={title}
+                        onChange={editTitle}
+                    />
+                    {showSave
+                        ?<IconButton size="small" onClick={saveTask}>
+                            <SaveIcon fontSize="inherit"/>
+                        </IconButton>
+                        :<></>
+                    }
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <Box>
+                        <Typography
+                            sx={{
+                                display: 'inline-block',
+                                border: '1px solid gray',
+                                color: 'secondary.contrastText',
+                                backgroundColor: 'secondary.main',
+                                textTransform: 'uppercase',
+                                pt: 0.2,
+                                //pb:0.2,
+                                pl: 1.5,
+                                pr: 1.5
+                            }}
+                        >
+                            {task.project.organization.organizationName}
+                        </Typography>
+                        <Typography
+                            sx={{
+                                display: 'inline-block',
+                                border: '1px solid gray',
+                                color: 'primary.contrastText',
+                                backgroundColor: 'primary.main',
+                                //textTransform:'uppercase',
+                                pt: 0.2,
+                                //pb:0.2,
+                                pl: 1.5,
+                                pr: 1.5,
+                                ml: 1.5
+                            }}
+                        >
+                            {task.project.projectName}
+                        </Typography>
+                    </Box>
+                    <input
+                        type="text"
+                        style={{
+                            fontSize: 'larger',
+                            border: 'none transparent',
+                            outline: 'none',
+                            marginLeft: '5px',
+                            textAlign:"right"
+                        }}
+                        value={time}
+                        onChange={editTime}
+                    />
+                    {showTimeSave
+                        ?<IconButton size="small" onClick={saveTaskTime}>
+                            <SaveIcon fontSize="inherit"/>
+                        </IconButton>
+                        :<></>
+                    }
                 </Box>
                 <Box>
-                    {"Organization: " +task.project.organization.organizationName}
-                    {"   Project: " +task.project.projectName}
-                </Box>
-                <Box>
-                    {task.description}
+                    <TextField
+                        id="outlined-multiline-flexible"
+                        label="Description"
+                        sx={{
+                            width: '100%'
+                        }}
+                        multiline
+                        rows={7}
+                        value={desc}
+                        onChange={editDesc}
+                    />
                 </Box>
                 {/*<Timeline>*/}
                 {/*    {task.history.map((historyItem)=>*/}
