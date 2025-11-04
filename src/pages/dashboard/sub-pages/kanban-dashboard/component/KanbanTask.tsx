53171b4d6700f0692cd21a8b8bc7584616b1a98c
Ivan N. <flashniv@gmail.com>
2025-11-04 19:12:44 +0200

    Kanban Part 3

--- src/pages/dashboard/sub-pages/kanban-dashboard/component/KanbanTask.tsx
+++ src/pages/dashboard/sub-pages/kanban-dashboard/component/KanbanTask.tsx
@@ -1,5 +1,73 @@
-import { Box, Paper } from "@mui/material";
+import { Box, IconButton, Menu, MenuItem, Paper } from "@mui/material";
 import { Task } from "../../../../../type/Task";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useState } from "react";
+
+interface KanbanTaskHeaderProps {
+    task: Task
+}
+
+function KanbanTaskHeader(props: KanbanTaskHeaderProps) {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    function getTime(task: Task): number {
+        let time = 0;
+
+        task.timeRecords?.forEach(timeRecord => {
+            if (timeRecord.accountingPeriod.open) {
+                time += timeRecord.duration;
+            }
+        });
+
+        return time;
+    }
+
+    return (
+        <Box
+            display={"flex"}
+            justifyContent={"space-between"}
+        >
+            <Box sx={{ textTransform: "uppercase" }}>
+                {props.task.project?.client?.name} - {props.task.project?.name}
+            </Box>
+            <Box>
+                {getTime(props.task)}
+                <IconButton
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                    sx={{ padding: "1px" }}
+                >
+                    <MoreVertIcon fontSize="small" />
+                </IconButton>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    slotProps={{
+                        list: {
+                            'aria-labelledby': 'basic-button',
+                        },
+                    }}
+                >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
+            </Box>
+        </Box>
+    );
+}
 
 interface KanbanTaskProps {
     task: Task
@@ -10,11 +78,7 @@ export default function KanbanTask(props: KanbanTaskProps) {
         <Paper
             sx={{ p: 1 }}
         >
-            <Box
-                sx={{ textTransform: "uppercase" }}
-            >
-                {props.task.project?.client?.name} - {props.task.project?.name}
-            </Box>
+            <KanbanTaskHeader task={props.task} />
             <Box pt={1}>
                 {props.task.name}
             </Box>
